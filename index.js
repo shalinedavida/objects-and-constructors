b81dd49dbb2a30b98d2d8a5c2090d10cc13f4f87
Shaline Davida <shalinedavida@gmail.com>
2025-05-01 19:55:25 +0300

    changed file contents

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 //   1.Create a User object with properties like name, stepsWalked (an array of daily steps), 
 // and a method totalSteps() that calculates and returns the total number of steps walked. Add another method averageSteps() that returns the average steps per day.
+// Pseudocode.
+// Create an object user .
+// Add the attributes which are the name and stepsWalked.
+// add a method totalSteps() which has an anonymus function .
+// Declare a variable sum and iterate through the array to print out the summation of total steps walked.
+// Create a method to calculate the average of the total steps taken which is done by dividing the summation of total steps taken to the lenyth of the stepswalked array.
+ const user={
+      name:'Jenna',
+      stepsWalked :[2000,4000,6000,8000],
 
-function User(name,stepsWalked){
-   this.name=name;
-   this.stepsWalked=stepsWalked;
-   this.totalSteps= function(){
-      let sum = 0
-      return stepsWalked.reduce((num, acc)=> acc+num, 0);
-   }
-   this.averageSteps = function(){
+   totalSteps :function(){
+      let sum =0
+       for(let i=0;i < user.stepsWalked.length;i++){
+         sum += user.stepsWalked[i].steps
+        return sum 
+   
+      }
+   },
+   averageSteps :function(){
       return this.totalSteps()/this.stepsWalked.length
    }
 };
-const user= new User('Jenna',[2000,4000,6000,8000]);
-console.log({user});
+console.log(user.name);
 console.log(user.stepsWalked);
-console.log(user.totalSteps());
-console.log(user.averageSteps());
+user.totalSteps();
+user.averageSteps();
 
 // 2. Create a Recipe constructor with name, ingredients (array), and cookTime in minutes. 
 // Add a method displayRecipe() that logs the name and all ingredients in a readable list, 
 // and another method isQuickMeal() that returns true if cookTime is 30 minutes or less.
+// Pseudocode.
+// create  a function Recipe that has the properties name,ingredients,cookTime.
+// add the method that displays the name and ingredients passed in your anonymus function .
+// iterate through the ingredients array using a forEach loop to log out the elements in the array.
+// create another method that uses the if else loop to print out the minutes that meets the condition needed.
+// log  out your output.
 function Recipe(name,ingredients,cookTime){
    this.name=name;
    this.ingredients=ingredients;
@@ -53,7 +68,10 @@ console.log(recipe.isQuickMeal());
 
 // 3. Create a Car object with properties like model, mileage, and serviceHistory (an array of service dates). 
 // Add a method addService(date) to add a new service record, and lastServiceDate() to return the most recent service date.
-
+// create a car object that takes in the model,mileage and serviceHistory properties.
+// use the push() method to add elements date  to the end of the serviceHistory array.
+// use the pop() method to return the last date in the array, which is the recent date .
+// log out to get the output.
 function Car(model, mileage, serviceHistory){
    this.model= model;
    this.mileage= mileage;
@@ -79,6 +97,12 @@ console.log(car.lastServiceDate());
 
 // 4.Create a Playlist object with a property songs (an array of song titles). 
 // Add methods addSong(title) to add a song, removeSong(title) to delete one, and listSongs() to log all songs currently in the playlist.
+// Pseudocode.
+// create an object playlist using the const variable.
+// add the songs which is an array properties and a method addSong .
+// use the push() method to add songs to the end of the songs array.
+// to remove a song from the array use the pop() method to remove the last song element or the shift() method to remove the first song element.
+// add the last method that logs out the songs in the current array of the songs array by using the array length.
  
  const playlist ={
    songs:['Dumebi','Awolo','Extra Pressure','Arike'],
@@ -106,6 +130,11 @@ console.log(car.lastServiceDate());
  
 // 5. Create a Course constructor with title, lessons (an array), and completedLessons (array). 
 // Add a method markComplete(lesson) that adds the lesson to completedLessons, and a method getProgress() that returns a string like "3 out of 5 lessons completed".
+// Pseudocode.
+// create a course constructor using the function constructor that takes in the title,lessons,and completedlessons.
+// create a method markcomplete that takes in a lesson .Using the unshift() method add a lesson to the beggining of  the array.
+// create a method that return the string of the lessons completed out of the lessons by creating a variable for the length of the completedlessons array and a variable for all the lesson array.
+// log out the output.
    function Course(title,lessons,completedLessons) {
       this.title = title;
       this.lessons= lessons;
@@ -117,15 +146,17 @@ console.log(car.lastServiceDate());
 
       }
       this.getProgress = function(){
-         return 
+         let  completelesson = this.completedLessons.length;
+         let lesson=this.lessons.length;
+         return (`${completelesson} out of ${lesson}completed`)
       }
-
       
    }
    const course = new Course ('English',['verbs','nouns','adjectives',],['nouns'])
    console.log(course.title);
    console.log(course.lessons);
    course.markComplete('pronouns');
+   course.getProgress();
  
 
    
